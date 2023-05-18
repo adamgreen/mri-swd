d45b481e813af45c5a5f749d00d44d61c82f1aa5
Adam Green <adamgreen@users.noreply.github.com>
2023-05-17 22:00:09 -0700

    Increase EXEC_RP2040_BOOT_ROM_FUNC_TIMEOUT_MS
    The 1 second timeout was much too short for calls to erase the FLASH
    when large sections were being requested for erasure.

--- config.h
+++ config.h
@@ -43,7 +43,7 @@
 #define READ_DHCSR_TIMEOUT_MS 1000
 
 // Timeout in milliseconds to use when executing RP2040 Boot ROM routines.
-#define EXEC_RP2040_BOOT_ROM_FUNC_TIMEOUT_MS 1000
+#define EXEC_RP2040_BOOT_ROM_FUNC_TIMEOUT_MS 30000
 
 
 // Set each of these to 1 or 0 to enable or disable error/debug logging for each of the modules.
